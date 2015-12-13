c31b972ddeba59d66ee03e57c81402fdf3013e90
tom--bo <otsuka.t.2013@gmail.com>
2015-12-13 07:17:02 +0000

    greedyPlayer増やす (1)

---
+++ SamurAImanager/players/greedyPlayer1.cpp
@@ -0,0 +1,4 @@
+double territoryMerits = 1;
+double selfTerritoryMerits = 0.3;
+double hurtingMerits = 100;
+double hidingMerits = 0.1;
---
+++ SamurAImanager/players/greedyPlayer2.cpp
@@ -0,0 +1,4 @@
+double territoryMerits = 0.3;
+double selfTerritoryMerits = 1;
+double hurtingMerits = 0.5;
+double hidingMerits = 1;
