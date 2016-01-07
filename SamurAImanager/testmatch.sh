69cff77927bcf2040cc36181bdca32dd408007a1
tom--bo <otsuka.t.2013@gmail.com>
2016-01-07 13:21:04 +0000

    greedy0,1,2

--- SamurAImanager/testmatch.sh
+++ SamurAImanager/testmatch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 manager/gameManager \
-    -a players/greedyPlayer -p "" -u "" -n "greedy1" -r 1 -s 100 \
-    -a players/greedyPlayer -p "" -u "" -n "greedy2" -r 2 -s 98 \
-    -a players/greedyPlayer -p "" -u "" -n "greedy3" -r 3 -s 80 \
-    -a players/greedyPlayer -p "" -u "" -n "random4" -r 4 -s 70 \
-    -a players/greedyPlayer -p "" -u "" -n "random5" -r 5 -s 60 \
-    -a players/greedyPlayer  -p "" -u "" -n "random6" -r 6 -s 20 \
+    -a players/greedyPlayer0 -p "" -u "" -n "greedy0" -r 1 -s 100 \
+    -a players/greedyPlayer1 -p "" -u "" -n "greedy1" -r 2 -s 98 \
+    -a players/greedyPlayer2 -p "" -u "" -n "greedy2" -r 3 -s 80 \
+    -a players/greedyPlayer0 -p "" -u "" -n "p0" -r 4 -s 70 \
+    -a players/greedyPlayer1 -p "" -u "" -n "p1" -r 5 -s 60 \
+    -a players/greedyPlayer2  -p "" -u "" -n "p2" -r 6 -s 20 \
     -t
 
 rm SamuraiLog*
