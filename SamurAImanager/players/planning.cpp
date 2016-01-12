525608e791bc0f82515658f88be9a60f7c4b4356
tom--bo <otsuka.t.2013@gmail.com>
2016-01-12 04:33:29 +0000

    Zouki1提出

--- SamurAImanager/players/planning.cpp
+++ SamurAImanager/players/planning.cpp
@@ -97,12 +97,13 @@ struct PlanningPlayer: Player {
     void setMerits(GameInfo& info){
         switch(info.weapon){
             case 0:
-                territoryMerits = 1;
+                territoryMerits = 2;
                 selfTerritoryMerits = 0.1;
                 hurtingMerits = 100;
                 hidingMerits = 0.2;
                 avoidingMerits = -1;
-                movingMerits = 1;               
+                movingMerits = 0.2;               
+                doubleMerits = 50;
                 break;
             case 1:
                 territoryMerits = 2;
@@ -111,15 +112,16 @@ struct PlanningPlayer: Player {
                 hidingMerits = 2;
                 avoidingMerits = -5;
                 movingMerits = 0.5;               
-                
+                doubleMerits = 50;
                 break;
             case 2:
-                territoryMerits = 1;
+                territoryMerits = 2;
                 selfTerritoryMerits = 0.1;
                 hurtingMerits = 100;
                 hidingMerits = 1;
                 avoidingMerits = -4;
-                movingMerits = 0.1;               
+                movingMerits = 0.5;               
+                doubleMerits = 50;
                 break;
         }
     }
