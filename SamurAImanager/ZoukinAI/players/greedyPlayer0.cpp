30800329f6ef7ebb4239b77eb3e03b079916cf3c
Your Name <you@example.com>
2016-01-12 18:07:18 +0900

    copuy for makesip

--- SamurAImanager/ZoukinAI/players/greedyPlayer0.cpp
+++ SamurAImanager/ZoukinAI/players/greedyPlayer0.cpp
@@ -1,7 +1,40 @@
-double territoryMerits = 1;
-double selfTerritoryMerits = 0.1;
-double hurtingMerits = 100;
-double hidingMerits = 0.2;
-double avoidingMerits = -1;
-double movingMerits = 1;
-double doubleMerits = 50;
+double territoryMerits;
+double selfTerritoryMerits;
+double hurtingMerits;
+double hidingMerits;
+double avoidingMerits;
+double movingMerits;
+double doubleMerits;
+void setMerits(int weaponid){
+    switch(weaponid){
+        case 0:
+            territoryMerits = 2;
+            selfTerritoryMerits = 0.1;
+            hurtingMerits = 100;
+            hidingMerits = 0.5;
+            avoidingMerits = -3;
+            movingMerits = 0.2;               
+            doubleMerits = 50;
+            break;
+        case 1:
+            territoryMerits = 2;
+            selfTerritoryMerits = 0.1;
+            hurtingMerits = 100;
+            hidingMerits = 2;
+            avoidingMerits = -5;
+            movingMerits = 0.5;               
+            doubleMerits = 50;
+            break;
+        case 2:
+            territoryMerits = 2;
+            selfTerritoryMerits = 0.1;
+            hurtingMerits = 100;
+            hidingMerits = 1;
+            avoidingMerits = -4;
+            movingMerits = 0.5;               
+            doubleMerits = 50;
+            break;
+    }
+}
+
+
