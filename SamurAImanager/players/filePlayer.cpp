698e7c2154912959051e0b373fd90d91aecc3ec8
osjupiter <water_totuka@yahoo.co.jp>
2016-02-01 00:18:53 +0900

    add fileplayer

---
+++ SamurAImanager/players/filePlayer.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+double enemyTerritoryMerits;
+double blankTerritoryMerits;
+double friendTerritoryMerits;
+double hurtingMerits;
+double hidingMerits;
+double avoidingMerits;
+double movingMerits;
+double centerMerits;
+double doubleMerits;
+
+
+double getIntFromFS(std::ifstream& ifs){
+    std::string str;
+    std::getline(ifs,str);
+    return std::stod(str);
+}
+
+void setMerits(int weaponid){
+    std::ifstream ifs("evolution/load.gen");
+    std::ofstream ofs("evolution/fileout.txt");
+    std::string str;
+    if (ifs.fail()){
+        ofs<<"load failed!!!!";
+        return;
+    }
+    ofs<<"open mid!!!";
+    enemyTerritoryMerits = getIntFromFS(ifs);
+    blankTerritoryMerits = getIntFromFS(ifs);;
+    friendTerritoryMerits = getIntFromFS(ifs);
+    hurtingMerits = getIntFromFS(ifs);
+    hidingMerits = getIntFromFS(ifs);
+    avoidingMerits = getIntFromFS(ifs);
+    movingMerits = getIntFromFS(ifs);
+    centerMerits = getIntFromFS(ifs);
+    doubleMerits = getIntFromFS(ifs);
+    
+}
--- SamurAImanager/testmatch.sh
+++ SamurAImanager/testmatch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 manager/gameManager \
-    -a players/greedyPlayer0 -p "" -u "" -n "greedy0" -r 1 -s 100 \
+    -a players/filePlayer -p "" -u "" -n "file" -r 1 -s 100 \
     -a players/greedyPlayer1 -p "" -u "" -n "greedy1" -r 2 -s 98 \
     -a players/greedyPlayer2 -p "" -u "" -n "greedy2" -r 3 -s 70 \
     -a players/greedyPlayer3 -p "" -u "" -n "greedy3" -r 3 -s 60 \
