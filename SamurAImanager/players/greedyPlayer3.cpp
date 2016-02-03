bd0959a98af92c00094e02e75c1c8ef266f86042
tom--bo <otsuka.t.2013@gmail.com>
2016-02-03 11:13:35 +0000

    fix indent

--- SamurAImanager/players/greedyPlayer3.cpp
+++ SamurAImanager/players/greedyPlayer3.cpp
@@ -9,13 +9,13 @@ double centerMerits;
 double doubleMerits;
 
 void setMerits(int weaponid){
-    enemyTerritoryMerits = 2;
-    blankTerritoryMerits = 1;
+    enemyTerritoryMerits  = 2;
+    blankTerritoryMerits  = 1;
     friendTerritoryMerits = 0.1;
-    hurtingMerits = 100;
-    hidingMerits = 0.1;
-    avoidingMerits = 10;
-    movingMerits = 0.2;
-    centerMerits = 0.2;
-    doubleMerits = 50;
+    hurtingMerits         = 100;
+    hidingMerits          = 0.1;
+    avoidingMerits        = 10;
+    movingMerits          = 0.2;
+    centerMerits          = 0.2;
+    doubleMerits          = 50;
 }
